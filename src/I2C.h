bc9ef3c8749ad8ab6a9ddc8880ee20168e560d7a
williamg42 <willig2@vt.edu>
2018-04-26 14:05:04 -0400

    Update I2C.h

--- src/I2C.h
+++ src/I2C.h
@@ -37,6 +37,7 @@ public:
     uint8_t read_byte(uint8_t);
     uint8_t read_only();
     uint8_t write_byte(uint8_t, uint8_t);
+    uint8_t write_byte(uint8_t);
     uint8_t read_length(uint8_t, uint8_t, uint8_t*);
 
 private:
